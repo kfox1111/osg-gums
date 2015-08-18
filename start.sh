56cef7c270e35a79a8839a5a7b100e430d0b60fc
Kevin Fox <Kevin.Fox@pnnl.gov>
2015-08-18 14:52:55 -0700

    Add some config bits...

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -e
 
+: ${MYSQL_HOST:=localhost}
+: ${MYSQL_PORT:=3306}
+: ${MYSQL_DB:=GUMS_1_3}
+: ${MYSQL_USER:=gums}
+: ${MYSQL_PASSWD:=mysecret}
+: ${STORE_CONFIG:=false} 
+: ${parameter_name:=value}
+
+sed -i "s/\(storeConfig=\).*/\1'$STORE_CONFIG'" /etc/gums/gums.config
+sed -i "s/\(hibernate.connection.username=\).*/\1'$MYSQL_USER'" /etc/gums/gums.config
+sed -i "s/\(hibernate.connection.url=\).*/\1'jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/$MYSQL_DB'/" /etc/gums/gums.config
+sed -i "s/\(hibernate.connection.password=\).*/\1'$MYSQL_PASSWD'/>" /etc/gums/gums.config
+
 /etc/init.d/tomcat6 start
