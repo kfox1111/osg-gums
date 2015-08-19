b171d0da7aad34e4ae80283975456d36e03d0e7b
Kevin Fox <Kevin.Fox@pnnl.gov>
2015-08-18 17:27:55 -0700

    Fix some script badness. Force time back so that db sync's happen right.

--- start.sh
+++ start.sh
@@ -8,9 +8,12 @@
 : ${STORE_CONFIG:=false} 
 : ${parameter_name:=value}
 
-sed -i "s/\(storeConfig=\).*/\1'$STORE_CONFIG'" /etc/gums/gums.config
-sed -i "s/\(hibernate.connection.username=\).*/\1'$MYSQL_USER'" /etc/gums/gums.config
-sed -i "s/\(hibernate.connection.url=\).*/\1'jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/$MYSQL_DB'/" /etc/gums/gums.config
-sed -i "s/\(hibernate.connection.password=\).*/\1'$MYSQL_PASSWD'/>" /etc/gums/gums.config
+sed -i "s/\(storeConfig=\).*/\1'$STORE_CONFIG'/" /etc/gums/gums.config
+sed -i "s/\(hibernate.connection.username=\).*/\1'$MYSQL_USER'/" /etc/gums/gums.config
+sed -i "s/\(hibernate.connection.url=\).*/\1'jdbc:mysql:\/\/$MYSQL_HOST:$MYSQL_PORT\/$MYSQL_DB'/" /etc/gums/gums.config
+sed -i "s/\(hibernate.connection.password=\).*/\1'$MYSQL_PASSWD'\/>/" /etc/gums/gums.config
+
+#Force timestamp on gums config into the past, so it will not be copied to the database, but so that the db's copy will be copied back to the node.
+touch -t 201212121212.12 /etc/gums/gums.config
 
 /etc/init.d/tomcat6 start
